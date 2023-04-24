1fe8e4b8afb186edf8675e2ef41a39aabbbb55fb
Manvir Mann <manvirm29@gmail.com>
2023-04-23 22:32:12 -0700

    gradient descent x^2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Used in ML to opitimize weights and biases
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Use the function x^2
 def y_function(x):
     return x ** 2
 
@@ -15,5 +16,18 @@ def y_derivative(x):
 x = np.arange(-100, 100, 0.1)
 y = y_function(x)
 
+current_pos = (80, y_function(80))
+
+# Rate at which algo updates itself
+learning_rate = 0.001
+
+for _ in range(1000):
+    # Minus because we are reversing direction
+    new_x = current_pos[0] - learning_rate * y_derivative(current_pos[0])
+    new_y = y_function(new_x)
+    current_pos = (new_x, new_y)
+
 plt.plot(x, y)
-plt.show()
+plt.scatter(current_pos[0], current_pos[1], color="red")
+plt.pause(0.001)
+plt.clf()
